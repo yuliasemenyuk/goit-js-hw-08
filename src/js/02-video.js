3eb9cb016f6400f9e50c3dd365accbe49bcdc7d2
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-23 01:09:11 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,9 +6,7 @@ const iframePlayer = new Player(iframe);
 const TIME_KEY = "videoplayer-current-time"
 const currentTime = localStorage.getItem(TIME_KEY);
 
-
 iframePlayer.setCurrentTime(currentTime);
-
 iframePlayer.on('timeupdate', throttle(data => localStorage.setItem(TIME_KEY, data.seconds), 1000));
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const formInputMail = document.querySelector('.feedback-form input');
+const formInputText = document.querySelector('.feedback-form textarea')
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+const FORM_KEY = 'feedback-form-state';
+const formData = {};
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.target.reset();
+    console.log(FORM_KEY, JSON.parse(localStorage.getItem(FORM_KEY)));
+    localStorage.removeItem(FORM_KEY);
+}
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+function savingValue() {
+        const savedMessage = JSON.parse(localStorage.getItem(FORM_KEY));
+    
+        if (savedMessage.message) {
+            formInputText.value = savedMessage.message;
+        };
+        
+        if (savedMessage.email) {
+            formInputMail.value = savedMessage.email;
+        };
+    };
+
+    savingValue();  
