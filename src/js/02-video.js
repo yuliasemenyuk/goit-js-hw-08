7f9d8d5940134ba213882eaa7b6ce87da3695024
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-22 20:01:06 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+const TIME_KEY = "videoplayer-current-time"
+const currentTime = localStorage.getItem(TIME_KEY);
+
+
+iframePlayer.setCurrentTime(currentTime);
+
+iframePlayer.on('timeupdate', throttle(data => localStorage.setItem(TIME_KEY, data.seconds), 1000));
+
 
