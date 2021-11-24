98ff736ccc2c9219daf87f9e77e5fbae280760c6
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-25 01:11:35 +0200

    corr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,27 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const iframePlayer = new Player(iframe);
-const TIME_KEY = "videoplayer-current-time"
-const currentTime = localStorage.getItem(TIME_KEY);
 
-iframePlayer.setCurrentTime(currentTime);
-iframePlayer.on('timeupdate', throttle(data => localStorage.setItem(TIME_KEY, data.seconds), 1000));
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
+if (localStorage.getItem(TIME_KEY)) {
+    player.setCurrentTime(localStorage.getItem(TIME_KEY));
+}
 
+const onPlay = () => {
+    player
+        .getCurrentTime()
+        .then(function (seconds) {
+            localStorage.setItem(TIME_KEY, seconds);
+        })
+        .catch(function (error) {
+            console.log(error.name);
+            console.log(error.message);
+        });
+};
+
+const throttled = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttled);
