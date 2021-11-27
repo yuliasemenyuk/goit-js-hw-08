be8a31737b2d789b40bb07e032927caeb8e90957
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-27 18:15:52 +0200

    form orrected

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,27 +2,52 @@ import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
 const formInputMail = document.querySelector('.feedback-form input');
-const formInputText = document.querySelector('.feedback-form textarea')
+const formInputText = document.querySelector('.feedback-form textarea');
 
 feedbackForm.addEventListener('submit', onFormSubmit);
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 
 const FORM_KEY = 'feedback-form-state';
-const formData = {};
+
+
+const savedFormDataJSON = localStorage.getItem(FORM_KEY);
+const savedFormData = JSON.parse(savedFormDataJSON);
+
+if (savedFormData !== null) {
+    formInputMail.value = savedFormData.email;
+    formInputText.value = savedFormData.message;
+};
 
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    evt.target.reset();
-    console.log(FORM_KEY, JSON.parse(localStorage.getItem(FORM_KEY)));
+    console.log(formInputMail.value);
+
+    if ((formInputMail.value === '') || (formInputText.value === '')) {
+        window.alert('Please, fill in the form');
+        return;
+
+    }
+    
+    const formData = { email: `${formInputMail.value}`, message: `${formInputText.value}` };
+    console.log(formData);
+
+    
     localStorage.removeItem(FORM_KEY);
+    formInputMail.value = "";
+    formInputText.value = "";
+ 
 }
 
-function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
-}
 
+function onFormInput() {
+
+        const formData = { email: `${formInputMail.value}`, message: `${formInputText.value}` };
+        const formDataJSON = JSON.stringify(formData);
+    
+        localStorage.setItem(FORM_KEY, formDataJSON);
+    
+    }
 function savingValue() {
         const savedMessage = JSON.parse(localStorage.getItem(FORM_KEY));
     
@@ -34,5 +59,9 @@ function savingValue() {
             formInputMail.value = savedMessage.email;
         };
     };
+    savingValue(); 
 
-    savingValue();  
+    
+
+    
+     
